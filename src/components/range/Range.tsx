f39c20ae19ca1d2408fd9a286ff2663ffce73d95
Joel Paucarima Franco <joel.dev.3112@gmail.com>
2023-05-24 18:42:16 +0200

    test: comment 2

--- src/components/range/Range.tsx
+++ src/components/range/Range.tsx
@@ -2,7 +2,18 @@ import { IRange } from '@/models/Range';
 import { RangeProvider } from '@/contexts/Range.context';
 import { RangeSlider } from '@/components/range/RangeSlider';
 
+// TODO: arreglo
 export const Range = (props: IRange) => {
+  return (
+    <RangeProvider {...props}>
+      <div className="range">
+        <RangeSlider />
+      </div>
+    </RangeProvider>
+  );
+};
+
+export const Range2 = (props: IRange) => {
   return (
     <RangeProvider {...props}>
       <div className="range">
